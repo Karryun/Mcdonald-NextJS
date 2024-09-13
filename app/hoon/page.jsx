91ae38bf4af9953daaa107a2303d9a1ffa3d3f59
DongHoon Lee <67902252+bluelemon61@users.noreply.github.com>
2024-09-13 16:02:51 +0900

    feat: 내꼬 완송

--- app/hoon/page.jsx
+++ app/hoon/page.jsx
@@ -1,8 +1,29 @@
+"use client"
+
+import Styles from "./hoon.module.css";
+import { useState } from "react";
 import Link from "next/link";
 
 export default function Hoon() {
+  const [drink, setDrink] = useState(1);
+
   return (
-    <div><Link href="/hoon/apple">hihihi</Link></div>
-    
+    <div className={Styles.Container}>
+      <button 
+        className={Styles.Drink}
+        onClick={() => {
+          if (drink < 10) setDrink(drink + 1);
+        }}
+      >
+        포카리 스윁
+      </button>
+      <input value={'🍹'.repeat(drink)} disabled />
+      <div className={Styles.ButtonArea}>
+        <button onClick={()=>setDrink(1)}>Reset</button>
+        <Link href='/'>Home</Link>
+      </div>
+
+      <p className={Styles.toChange}>안녕하세요 css를 받고싶어요.</p>
+    </div>
   )
 }
