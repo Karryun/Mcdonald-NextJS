3de163c267bc2e13523e847d29d91bcd1cb10ac6
soheean1370 <127065983+soheean1370@users.noreply.github.com>
2024-10-04 22:16:09 +0900

    실습

--- app/sohee/page.jsx
+++ app/sohee/page.jsx
@@ -2,7 +2,10 @@
 
 import Link from "next/link";
 import styles from './page.module.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ProgressBar from '../components/ProgressBar';
+import axios from "axios";
+
 
 export default function Sohee() {
     const [choco, setChoco ] = useState(1);
@@ -10,12 +13,68 @@ export default function Sohee() {
     const AddChoco = () => {
         if(choco < 22){
             setChoco(choco + 1);
+            setPeopleProgress(icon + 1);
         }
     }
 
     const reset = () => {
         setChoco(1); // 초코 상태를 1로 초기화
     }
+    
+
+    const [peopleProgress, setPeopleProgress] = useState({
+        jake: {
+            progress: 1,
+            icon: '',
+        },
+        hoon : {
+            progress: 1,
+            icon: '',
+        },
+        sang : {
+            progress: 1,
+            icon: '',
+        },
+        karryun: {
+            progress : 1,
+            icon: '',
+            
+        }
+    }); // 객체라서 똑같이 객체로 초기화
+
+    const [text, setText] = useState("없어요");
+    const [tempText, setTempText] = useState("");
+    const clicked = () => {
+      axios.get("http://127.0.0.1:8000/polls/get/", {
+        params: {
+          abc:tempText,
+        },
+      })
+      .then((response) => setText(JSON.stringify(response.data)))
+      .then(() => {setTempText("")});
+    }
+
+    const onChange = (e) => {
+      setTempText(e.target.value);
+    }
+
+    const dataFetcher = async () => { //api 핸들링하는 비동기 함수
+        const res = await fetch('/api/progress',{
+            method: 'GET',
+        })
+
+        const jsonData = await res.json(); //비동기
+
+        setChoco(jsonData.sohee.progress);
+        setPeopleProgress(jsonData);
+
+        console.log(jsonData);
+    }
+
+    useEffect( () => {
+        dataFetcher();
+      }, []);
+
 
     return (
       <div className={styles.head}> Sweet Chocolate🍪
@@ -32,6 +91,20 @@ export default function Sohee() {
                 </button>
             </div>
         </div>
+        <div className={styles.container}>
+        <ProgressBar name = 'jake' icon = {peopleProgress.jake.icon}/>
+        <ProgressBar name = 'hoon' icon = {peopleProgress.hoon.icon}/>
+        <ProgressBar name = 'sang' icon = {peopleProgress.sang.icon}/>
+        <ProgressBar name = 'karryun' icon = {peopleProgress.karryun.icon}/>
+
+        </div>
+        <div>
+          <input id="myText" type="text" onChange={onChange} value={tempText}/>
+          <button onClick = {clicked}>너굴 버튼</button>
+          <h1>{text}</h1>
+        </div>
+
       </div>
+
     );
 };
